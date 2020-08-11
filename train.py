a4cc7a0555cd761c876c6d1826ecc0baf67d5b85
Dilip Thiagarajan <dthiagarajan@github.com>
2020-08-11 16:52:13 -0400

    Set early stopping to true by default

--- train.py
+++ train.py
@@ -23,7 +23,7 @@ def get_args():
         'Train a classification model on PTB-XL diagnostic superclass'
     )
     parser.add_argument('--checkpoint_models', type=bool, default=False)
-    parser.add_argument('--early_stopping', type=bool, default=False)
+    parser.add_argument('--early_stopping', type=bool, default=True)
     parser.add_argument('--find_lr', type=bool, default=False)
     parser.add_argument('--show_heatmaps', type=bool, default=False)
     parser = PTBXLClassificationModel.add_model_specific_args(parser)
